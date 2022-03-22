dc28cb62aa394d4d77ce5e3ae9e38221efc51e1b
Arefyev Anton <ant.ar9809@gmail.com>
2022-03-22 20:48:02 +0300

    [add] 02_repeatString solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, count) {
+    let result = "";
+    if (count < 0) {
+        result = "ERROR";
+    }
+    else {
+        for (let index = 0; index < count; index++) {
+            result += string;        
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
