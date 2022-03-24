d8646a6149e5da31da0b148148bee6441fa3d44f
Arefyev Anton <ant.ar9809@gmail.com>
2022-03-24 21:41:34 +0300

    [add] 07_tempConversion solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let result = (temp - 32) * 5 / 9;
+  return Number(result.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let result = temp * 9 / 5 + 32;
+  return Number(result.toFixed(1));
 };
+
+console.log(ftoc(100));
+console.log(ctof(0));
 
 // Do not edit below this line
 module.exports = {
