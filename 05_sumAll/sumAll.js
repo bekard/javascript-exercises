1acb16a060ba703a4dcd6e2edb767c3beada63a2
Arefyev Anton <ant.ar9809@gmail.com>
2022-03-24 08:09:30 +0300

    [add] 05_sumAll solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+function validArguments(first, second) {
+    return (first >= 0 && second >= 0) &&
+        (Number.isInteger(first) && Number.isInteger(second));
+}
 
+function calculate(first, second) {
+    let result = 0;
+    if (first > second) {
+        const temp  = first;
+        first = second;
+        second = temp;
+    }
+    for (let value = first; value <= second; value++) {
+        result += value;
+    }
+    return result;    
+}
+
+const sumAll = function(first, second) {
+    return validArguments(first, second) ? calculate(first,second) : "ERROR";
 };
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
