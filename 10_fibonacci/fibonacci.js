97840eeb8a9d85c427a3c662a876025d4dff90c9
ant.ar9809 <ant.ar9809@gmail.com>
2022-04-07 22:22:19 +0300

    [add] 10_fibonacci solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(value) {
+    value = Number(value);
+    if (value >= 0) {
+        if (value === 0) {
+            return 0;
+        }
+        else if (value === 1) {
+            return 1;
+        }
+        return fibonacci(value - 2) + fibonacci(value - 1);
+    }
+    return "OOPS";
 };
+
+console.log(fibonacci(4));
 
 // Do not edit below this line
 module.exports = fibonacci;
