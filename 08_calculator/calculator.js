a4b445db5c2814065d060fab1d4c0f3320d85f89
ant.ar9809 <ant.ar9809@gmail.com>
2022-04-07 08:42:25 +0300

    [add] calculator solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	const sum = array.reduce(
+    (previosValue, currentValue) => previosValue + currentValue, 0
+  );
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  const multi = array.reduce(
+    (prevValue, currentValue) => prevValue * currentValue, 1
+  );
+  return multi;
 };
 
-const power = function() {
-	
+const power = function(value, pow) {
+	return value ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(value) {
+  let res = 1;
+	for (let index = 1; index <= value; index++) {
+    res *= index;
+  }
+  return res;
 };
 
 // Do not edit below this line
