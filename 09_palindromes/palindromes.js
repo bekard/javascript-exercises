ab470822a461df99d04f3fc318f72f2a4dec921b
ant.ar9809 <ant.ar9809@gmail.com>
2022-04-07 22:07:54 +0300

    [add]  09_palindromes solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,27 @@
-const palindromes = function () {
-
+/*
+const palindromes = function (string) {
+    string = string.toLowerCase().replace(/[^a-z]/g, "");
+    let begin = 0;
+    let end = string.length - 1;
+    for(; begin < end; ) {
+        if (string[begin++] != string[end--]) {
+            return false;
+        }
+    }
+    return true;
 };
+*/
+
+const palindromes = function (string) {
+    processedString = string.toLowerCase().replace(/[^a-z]/g, "");
+    return (
+        processedString
+            .split("")
+            .reverse()
+            .join("") == processedString
+    );
+};
+
 
 // Do not edit below this line
 module.exports = palindromes;
