12e898ed58958b44dac60f9d41cc90104ca616d7
ant.ar9809 <ant.ar9809@gmail.com>
2022-04-08 08:03:50 +0300

    [add]  11_getTheTitles solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const getPersonAge = function(person) {
+    return ("yearOfDeath" in person ? person.yearOfDeath - person.yearOfBirth
+        : new Date().getFullYear() - person.yearOfBirth);
+}
 
+const findTheOldest = function(persons) {
+    persons.sort(
+        (firstPerson, secondPerson) => {
+            firstAge = getPersonAge(firstPerson);
+            secondAge = getPersonAge(secondPerson);
+            return firstAge > secondAge ? -1 : 1;
+        }
+    )
+    return persons[0];
 };
 
 // Do not edit below this line
